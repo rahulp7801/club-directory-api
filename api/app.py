9c6078540a64fac997e859ccc58ca56994df9b55
Rahul P <76501505+rahulp7801@users.noreply.github.com>
2024-08-13 17:12:50 -0700

    add login

--- api/app.py
+++ api/app.py
@@ -2,14 +2,15 @@ import os
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from utilities import init_curs
-from user_util import User
 from club_util import VistaClubLookup
+from user_util import verify_login
 
 app = Flask(__name__)
 CORS(app)
 
 # Configuration
 SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
+print(SECRET_KEY)
 
 @app.before_request
 def before_request():
@@ -17,21 +18,14 @@ def before_request():
 
 @app.route("/api/login-google", methods=["POST"])
 def login_google():
-    print(request.json)
-    user = User(request.json)
-    res, stat = user.reg_user()
-    if not res:
-        return jsonify({'message': 'Internal error' if stat == 401 else 'User does not exist'}), 400
-    if stat == 201:
-        return jsonify({'message': "New user, prompt for extra info"})
-    return jsonify({'message': 'Login successful'})
+    data = request.json
+    id_token = data.get("idToken")
 
-@app.route("/api/get-header-data", methods=["POST"])
-def get_header_data():
-    print(request.json)
-    user = User((request.json)['token'])
-    print(user.get_header_data())
-    return user.get_header_data()
+    if not id_token:
+        return jsonify({'error': 'ID token is required'}), 400
+    
+    return verify_login(id_token)
+
 
 @app.route("/api/get-clubs-list", methods=['GET'])
 def get_classes_list():
