b0100fc74ffb6cf72b7c07cb29010bf5f9bfa37a
Rahul P <76501505+rahulp7801@users.noreply.github.com>
2024-08-18 01:22:31 -0700

    please

--- api/user_util.py
+++ api/user_util.py
@@ -1,28 +1,28 @@
 from time import time
-import firebase_admin
-from firebase_admin import db, credentials, auth
+from firebase_admin import initialize_app
+from firebase_admin.credentials import Certificate
+from firebase_admin.auth import InvalidIdTokenError, ExpiredIdTokenError, verify_id_token
 from dotenv import load_dotenv
 
 from flask import jsonify
 
-cred = credentials.Certificate("api/creds.json")
-firebase_admin.initialize_app(cred)
+cred = Certificate("api/creds.json")
+initialize_app(cred)
 
 
 from flask import jsonify
-from firebase_admin import auth
 
 def verify_login(id_token):
     try:
-        decoded_token = auth.verify_id_token(id_token)
+        decoded_token = verify_id_token(id_token)
         uid = decoded_token['uid']
         print("User authenticated")
         
         return jsonify({'message': 'User authenticated', 'uid': uid}), 200
 
-    except auth.InvalidIdTokenError:
+    except InvalidIdTokenError:
         return jsonify({'error': 'Invalid token'}), 401
-    except auth.ExpiredIdTokenError:
+    except ExpiredIdTokenError:
         return jsonify({'error': 'Token expired'}), 401
     except Exception as e:
         print(f'Error verifying token: {e}')
